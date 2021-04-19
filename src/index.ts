01dcbc3608e302dfb59f43a323d8f6226b8b37d0
doohyunlm <doohyunlm@svcorps.dev>
2021-04-19 15:34:03 +0900

    ## Add : TypeScript Class 활용 블록체인 만들기#1

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-interface Human {
-    name : string,
-    age : number,
-    gender : string
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number) {
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
+    }
 }
 
-const person = {
-    name: "doohyun",
-    age : 28,
-    gender : "man"
-    
-}
+const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
 
-const Hi = (person: Human):string => {
-return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(Hi(person));
+console.log(blockchain)
 
 export {};
