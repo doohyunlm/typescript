4d73fd1b0b8edf8f24024e3240235bb0256eddc8
doohyunlm <doohyunlm@svcorps.dev>
2021-04-19 15:47:24 +0900

    ## Add : TypeScript Class 활용 블록체인 만들기#2

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string
+        ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
         previousHash: string,
         data: string,
-        timestamp: number) {
+        timestamp: number
+        ) {
             this.index = index;
             this.hash = hash;
             this.previousHash = previousHash;
@@ -20,8 +31,12 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain)
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block =>  getBlockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
